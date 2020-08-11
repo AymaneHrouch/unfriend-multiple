52a187a7f701f265d43a177c17c1f819b438f14a
Aymane Hrouch <jo.aymane@Gmail.com>
2020-08-11 14:01:27 +0100

    Create app_drafts.js

---
+++ app_drafts.js
@@ -0,0 +1,9 @@
+/*
+
+// main function, old one, used to call it by the event listener using a for loop, but noticed that the browser freezes when there're a lot of friends to remove
+unfriend = (id) => {
+ 		friends[id].children[2].children[0].children[0].children[3].children[0].click()
+		document.querySelectorAll('[data-sigil="touchable touchable mflyout-remove-on-click m-unfriend-request"]')[0].click()
+		console.log	(friends[id].children[1].children[0].children[0].children[0].children[0].innerText + " has been unfriended")
+}
+*/
