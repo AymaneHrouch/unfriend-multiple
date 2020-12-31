250555977be7e2b1dcf38e24b9ce0e33681fbe77
Aymane Hrouch <jo.aymane@gmail.com>
2020-12-31 17:20:00 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
 // use the link https://mobile.facebook.com/[Your Username]/friends
 
-// scrolling
-const loader = document.querySelector(".seeMoreFriends");
-if (loader) {
-  const obs = new MutationObserver(callback);
-  const options = { childList: true };
-  obs.observe(document.querySelector("._2pit"), options);
-  createCB();
-}
-
-
-const callback = (mutationList, observer) => {
-  console.log("callback");
-  const moreFriends = document.querySelector(".seeMoreFriends");
-  if (moreFriends) {
-    createCB();
-  } else {
-    createCB();
-	console.log("no more friends")
-  }
-};
-
-/////
-
-toUnfriend = [];
-
 // CREATING THE CHECKBOXES //
 checkboxId = 0;
-createCB = () => {
+const createCB = () => {
   friends = document.querySelectorAll(
     '._55wp._7om2._5pxa._8yo0[data-sigil="undoable-action"]'
   );
@@ -37,36 +12,37 @@ createCB = () => {
     checkBox.style.height = "30px";
     checkBox.style.width = "30px";
     checkBox.style.margin = "auto";
+    checkBox.classList.add("mycheckbox");
     checkBox.id = checkboxId++;
     friends[i].appendChild(checkBox);
   }
-  createBtn();
 };
 
-// main function to unfriend // new version dusing setTimeout
-unfriend = (id, arr) => {
-  if (id < arr.length) {
-    setTimeout(function () {
-      friends[
-        arr[id]
-      ].children[2].children[0].children[0].children[3].children[0].click();
-      document
-        .querySelectorAll(
-          '[data-sigil="touchable touchable mflyout-remove-on-click m-unfriend-request"]'
-        )[0]
-        .click();
-      console.log(
-        friends[arr[id]].children[1].children[0].children[0].children[0]
-          .children[0].innerText + " has been unfriended"
-      );
-      friends[arr[id]].children[3].checked = false;
-      id++;
-      unfriend(id, arr);
-    }, 1000);
-  } else {
-    console.log("DONE!");
+/// Load Friends
+const loadFriends = (scrolling = false) => {
+  const callback = (mutationList, observer) => {
+    console.log("callback");
+    const moreFriends = document.querySelector(".seeMoreFriends");
+    if (moreFriends) {
+      createCB();
+      if (scrolling) moreFriends.scrollIntoView();
+    } else {
+      observer.disconnect();
+      console.log("no more friends");
+      createCB();
+    }
+  };
+
+  const loader = document.querySelector(".seeMoreFriends");
+  if (loader) {
+    const obs = new MutationObserver(callback);
+    const options = { childList: true };
+    obs.observe(document.querySelector("._2pit"), options);
+    createCB();
+    if (scrolling) loader.scrollIntoView();
   }
 };
+loadFriends();
 
 // CREATING THE BYE BUTTON //
 createBtn = () => {
@@ -76,16 +52,48 @@ createBtn = () => {
   list = document.querySelector("._55wo._55x2");
   list.insertBefore(btn, list.childNodes[0]);
   btn.addEventListener("click", () => {
-    for (i = 0; i < friends.length; i++) {
-      cB = document.getElementById(i).checked;
-      if (cB) {
-        toUnfriend.push(i);
-      }
-    }
-
-    unfriend(0, toUnfriend);
-    toUnfriend = [];
+    unfriend(0, toUnfriend());
   });
+};
+createBtn();
+
+// main function to unfriend // new version using setTimeout
+toUnfriend = () => {
+  friends = document.querySelectorAll(
+    '._55wp._7om2._5pxa._8yo0[data-sigil="undoable-action"]'
+  );
+  return Array.prototype.slice
+    .call(document.getElementsByClassName("mycheckbox"))
+    .filter(f => f.checked);
+};
+
+unfriend = (id, arr) => {
+  if (id >= arr.length) return;
+  friendIndex = arr[id].id;
+  friends = document.querySelectorAll(
+    '._55wp._7om2._5pxa._8yo0[data-sigil="undoable-action"]'
+  );
+  if (id < arr.length) {
+    setTimeout(function () {
+      friends[
+        friendIndex
+      ].children[2].children[0].children[0].children[3].children[0].click();
+      document
+        .querySelectorAll(
+          '[data-sigil="touchable touchable mflyout-remove-on-click m-unfriend-request"]'
+        )[0]
+        .click();
+      console.log(
+        friends[friendIndex].children[1].children[0].children[0].children[0]
+          .children[0].innerText + " has been unfriended"
+      );
+      friends[friendIndex].children[3].checked = false;
+      id++;
+      unfriend(id, arr);
+    }, 100);
+  } else {
+    console.log("DONE!");
+  }
 };
 
 // Select all
